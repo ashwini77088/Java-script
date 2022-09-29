cf89068966db23cda98b4dde973c32589871ce37
Ashwini <113336360+ashwini77088@users.noreply.github.com>
2022-09-29 18:01:32 +0530

    assignment

--- Assigment/array.js
+++ Assigment/array.js
@@ -2,3 +2,12 @@ let names=['adlin','guru','abi','gokul','raghul',]
 for (i=0;i<4;i=i+1){
     console.log(i)
 }
+
+//
+
+let c=[10,20,30,40,50]
+console.log(c[0])
+console.log(c[1])
+console.log(c[2])
+console.log(c[3])
+console.log(c[4])
