bf2ba092709b5aa27661f4e2f04c6449fa6bb45d
Ashwini <113336360+ashwini77088@users.noreply.github.com>
2022-09-29 19:27:52 +0530

    done

---
+++ Assigment/ans1.js
@@ -0,0 +1,10 @@
+let a=13
+if (a%3==0){
+    console.log('division');
+}
+else{
+    console.log('not divisible')
+}
+
+
+
---
+++ Assigment/ans2.js
@@ -0,0 +1,7 @@
+let a=450
+if(a*3==500){
+    console.log('multiple of 3');
+}
+else{
+    console.log("not mulitple of 3")
+}
