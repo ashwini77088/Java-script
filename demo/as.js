2ca4cdafdde5741b63aa7cb725d61f119fb1a785
Ashwini <113336360+ashwini77088@users.noreply.github.com>
2022-10-05 09:56:46 +0530

    done

---
+++ demo/as.js
@@ -0,0 +1,27 @@
+let a=1
+while (a<10){
+    console.log(a);
+    a=a+1
+}
+//-10 to1
+let b=10
+while (b>=1){
+    console.log(b)
+    b=b-1
+}
+//ascending order
+let array=[a,b,c]
+array.sort((a,b)=>a-b);
+console.log(array);
+
+//Assignment no : 12 Descending  Order
+array.sort((a,b)=>b-a);
+console.log(array);
+
+//demo
+var e=10
+var f=20
+console.log(e+f);
+
+
+
---
+++ demo/as1.js
@@ -0,0 +1,19 @@
+var e=10
+var f="hello"
+console.log(e+f);
+
+
+//demo
+var a=10
+var b=20
+console.log(a*b)
+
+
+//demo
+
+var c=10
+var d=20
+console.log(c % d)
+
+
+//demo
