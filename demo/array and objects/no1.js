bd0442eb7369b81fa2d4a8a4c77924c213529c3e
Ashwini <113336360+ashwini77088@users.noreply.github.com>
2022-10-07 09:44:42 +0530

    done

---
+++ demo/array and objects/no1.js
@@ -0,0 +1,23 @@
+let employee={
+    id   :   101,
+    name :   "anu",
+    salary:   40000
+}
+
+for (key in employee){               //print total key +value length
+    console.log(key.length);
+}
+
+
+//print the object
+console.log(employee)
+
+
+//print the keyof value of the object
+
+console.log(object(employee));
+//print key alone
+
+for(keys in employee){
+    console.log(keys);
+}
