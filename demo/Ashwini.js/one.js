cbd74f3e56c76094c71babb5746f30cefcd7b1fe
Ashwini <113336360+ashwini77088@users.noreply.github.com>
2022-10-10 20:55:26 +0530

    ok

---
+++ demo/Ashwini.js/one.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    console.log(a)
+    console.log(b)
+}
+add(10,20);
+
+
+//demo
+function add(a,b){
+    console.log(a+b);
+}
+add(10,20);
+add(20,30);
+add(100,200);
+add(1000)
