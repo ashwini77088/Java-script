14b3079210519f384442b7a8f5758fb1cb9d137c
Ashwini <113336360+ashwini77088@users.noreply.github.com>
2022-10-13 17:19:22 +0530

    done

---
+++ Assigment/Events assignment/data.js
@@ -0,0 +1,22 @@
+function wish() {
+    let tag_Ref = document.getElementById('efg').style.backgroundColor = "blue"
+}
+function add() {
+    let tag_Ref= document.getElementById('ghi').style.backgroundColor = "green"
+}
+function Ashwini() {
+    let  tag_Ref= document.getElementById('jkl').style.backgroundColor = "aqua"
+}
+function guru() {
+    let tag_Ref = document.getElementById('mno').style.backgroundColor = "blue"
+}
+function Adlin() {
+    let tag_Ref = document.getElementById('pqr').style.backgroundColor = "green"
+}
+function adlin() {
+    //alert("test")
+    let data=document.getElementById('stu').value
+  
+    document.getElementById('stu').value = document.getElementById('stu').value.toUpperCase()
+}
+  
